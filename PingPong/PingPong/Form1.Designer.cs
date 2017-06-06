495194137a9d1119a27b3e9632f705ca34fa1505
BiroMarton <rezoner9@gmail.com>
2017-06-06 20:59:28 +0200

    Added difficulty score and an image hopefully

--- PingPong/PingPong/Form1.Designer.cs
+++ PingPong/PingPong/Form1.Designer.cs
@@ -29,11 +29,20 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.PlayArea = new System.Windows.Forms.Panel();
+            this.victory = new System.Windows.Forms.PictureBox();
+            this.GameOver = new System.Windows.Forms.Label();
+            this.Pause = new System.Windows.Forms.Label();
+            this.Number = new System.Windows.Forms.Label();
+            this.Score = new System.Windows.Forms.Label();
             this.ball = new System.Windows.Forms.PictureBox();
             this.pad = new System.Windows.Forms.PictureBox();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.Diff = new System.Windows.Forms.Label();
+            this.DiffVal = new System.Windows.Forms.Label();
             this.PlayArea.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.victory)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.ball)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pad)).BeginInit();
             this.SuspendLayout();
@@ -41,19 +50,80 @@
             // PlayArea
             // 
             this.PlayArea.AutoSize = true;
-            this.PlayArea.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.PlayArea.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.PlayArea.Controls.Add(this.DiffVal);
+            this.PlayArea.Controls.Add(this.Diff);
+            this.PlayArea.Controls.Add(this.victory);
+            this.PlayArea.Controls.Add(this.GameOver);
+            this.PlayArea.Controls.Add(this.Pause);
+            this.PlayArea.Controls.Add(this.Number);
+            this.PlayArea.Controls.Add(this.Score);
             this.PlayArea.Controls.Add(this.ball);
             this.PlayArea.Controls.Add(this.pad);
             this.PlayArea.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.PlayArea.Enabled = false;
             this.PlayArea.Location = new System.Drawing.Point(0, 0);
+            this.PlayArea.Margin = new System.Windows.Forms.Padding(2);
             this.PlayArea.Name = "PlayArea";
-            this.PlayArea.Size = new System.Drawing.Size(545, 445);
+            this.PlayArea.Size = new System.Drawing.Size(1187, 684);
             this.PlayArea.TabIndex = 0;
+            // 
+            // victory
+            // 
+            this.victory.Image = ((System.Drawing.Image)(resources.GetObject("victory.Image")));
+            this.victory.Location = new System.Drawing.Point(716, 45);
+            this.victory.Name = "victory";
+            this.victory.Size = new System.Drawing.Size(471, 448);
+            this.victory.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.victory.TabIndex = 7;
+            this.victory.TabStop = false;
+            // 
+            // GameOver
+            // 
+            this.GameOver.AutoSize = true;
+            this.GameOver.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.GameOver.Location = new System.Drawing.Point(126, 382);
+            this.GameOver.Name = "GameOver";
+            this.GameOver.Size = new System.Drawing.Size(363, 33);
+            this.GameOver.TabIndex = 5;
+            this.GameOver.Text = "Game Over! Your score is:";
+            // 
+            // Pause
+            // 
+            this.Pause.AutoSize = true;
+            this.Pause.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Pause.Location = new System.Drawing.Point(48, 93);
+            this.Pause.Name = "Pause";
+            this.Pause.Size = new System.Drawing.Size(327, 55);
+            this.Pause.TabIndex = 4;
+            this.Pause.Text = "Game paused";
+            // 
+            // Number
+            // 
+            this.Number.AutoSize = true;
+            this.Number.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Number.Location = new System.Drawing.Point(116, 9);
+            this.Number.Name = "Number";
+            this.Number.Size = new System.Drawing.Size(31, 33);
+            this.Number.TabIndex = 3;
+            this.Number.Text = "0";
+            // 
+            // Score
+            // 
+            this.Score.AutoSize = true;
+            this.Score.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Score.Location = new System.Drawing.Point(3, 9);
+            this.Score.Name = "Score";
+            this.Score.Size = new System.Drawing.Size(107, 33);
+            this.Score.TabIndex = 2;
+            this.Score.Text = "Score :";
             // 
             // ball
             // 
-            this.ball.BackColor = System.Drawing.Color.Snow;
-            this.ball.Location = new System.Drawing.Point(222, 153);
+            this.ball.BackColor = System.Drawing.Color.DarkGreen;
+            this.ball.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.ball.Location = new System.Drawing.Point(197, 172);
+            this.ball.Margin = new System.Windows.Forms.Padding(2);
             this.ball.Name = "ball";
             this.ball.Size = new System.Drawing.Size(30, 30);
             this.ball.TabIndex = 1;
@@ -62,27 +132,52 @@
             // pad
             // 
             this.pad.BackColor = System.Drawing.SystemColors.Desktop;
-            this.pad.Location = new System.Drawing.Point(204, 403);
+            this.pad.Location = new System.Drawing.Point(153, 327);
+            this.pad.Margin = new System.Windows.Forms.Padding(2);
             this.pad.Name = "pad";
-            this.pad.Size = new System.Drawing.Size(150, 30);
+            this.pad.Size = new System.Drawing.Size(242, 24);
             this.pad.TabIndex = 0;
             this.pad.TabStop = false;
             // 
             // timer1
             // 
+            this.timer1.Interval = 10;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Diff
+            // 
+            this.Diff.AutoSize = true;
+            this.Diff.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Diff.Location = new System.Drawing.Point(3, 45);
+            this.Diff.Name = "Diff";
+            this.Diff.Size = new System.Drawing.Size(171, 33);
+            this.Diff.TabIndex = 9;
+            this.Diff.Text = "Ball Speed: ";
+            // 
+            // DiffVal
+            // 
+            this.DiffVal.AutoSize = true;
+            this.DiffVal.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.DiffVal.Location = new System.Drawing.Point(158, 45);
+            this.DiffVal.Name = "DiffVal";
+            this.DiffVal.Size = new System.Drawing.Size(31, 33);
+            this.DiffVal.TabIndex = 10;
+            this.DiffVal.Text = "3";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
-            this.ClientSize = new System.Drawing.Size(545, 445);
+            this.ClientSize = new System.Drawing.Size(1187, 684);
             this.Controls.Add(this.PlayArea);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
             this.PlayArea.ResumeLayout(false);
+            this.PlayArea.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.victory)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.ball)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pad)).EndInit();
             this.ResumeLayout(false);
@@ -96,6 +191,13 @@
         private System.Windows.Forms.PictureBox ball;
         private System.Windows.Forms.PictureBox pad;
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label Number;
+        private System.Windows.Forms.Label Score;
+        private System.Windows.Forms.Label Pause;
+        private System.Windows.Forms.Label GameOver;
+        private System.Windows.Forms.PictureBox victory;
+        private System.Windows.Forms.Label DiffVal;
+        private System.Windows.Forms.Label Diff;
     }
 }
 
--- PingPong/PingPong/Form1.cs
+++ PingPong/PingPong/Form1.cs
@@ -13,32 +13,89 @@ namespace PingPong
     public partial class Form1 : Form
 
     {
-        public int speed = 5;
+        public int speed_top = 3;
+        public int speed_left = 3;
         public int points = 0;
-        public int difficulty = 0;
+ 
 
         public Form1()
         {
             InitializeComponent();
 
             timer1.Enabled = true;
+
             Cursor.Hide();
             this.FormBorderStyle = FormBorderStyle.None;
             this.TopMost = true;
             this.Bounds = Screen.PrimaryScreen.Bounds;
 
             pad.Top = PlayArea.Bottom -(PlayArea.Bottom/10);
+            Pause.Left = (PlayArea.Width / 2) - (Pause.Width / 2);
+            Pause.Top = (PlayArea.Height/2) - (Pause.Height/2);
+            GameOver.Left = (PlayArea.Width / 2) - (GameOver.Width / 2);
+            GameOver.Top = (PlayArea.Height / 2) - (GameOver.Height / 2);
+            GameOver.Visible = false;
+
+            Pause.Visible = false;
+            victory.Visible = false;
+            victory.Left = PlayArea.Width - victory.Width;
+
+         
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
             pad.Left = Cursor.Position.X - (pad.Width / 2);
 
-            ball.Left += speed;
-            ball.Top += speed;
+            ball.Left += speed_left;
+            ball.Top += speed_top;
 
             if (ball.Bottom >= pad.Top && ball.Bottom <= pad.Bottom && ball.Left >= pad.Left && ball.Right <= pad.Right)
             {
+
+                if (points < 8) {
+                    speed_left += 2;
+                    speed_top += 2;
+                    DiffVal.Text = Math.Abs(speed_top).ToString();
+                }
+
+                if (points == 10 || points == 25) {
+                    pad.Width -= 50;
+                }
+
+
+          
+
+                speed_top= -speed_top;
+                points += 1;
+                Number.Text = points.ToString();
+            }
+
+            if (ball.Left <= PlayArea.Left || ball.Right >= PlayArea.Right)
+            {
+                speed_left = -speed_left;
+            }
+
+            if (ball.Top <= PlayArea.Top)
+            {
+                speed_top = -speed_top;
+            }
+
+            if (ball.Bottom >= PlayArea.Bottom)
+            {
+                timer1.Enabled = false;
+                GameOver.Visible = true;
+                Score.Visible = false;
+                Number.Left = (PlayArea.Width / 2) + (GameOver.Width/2);
+                Number.Top = GameOver.Top;
+                ball.Visible = false;
+            }
+
+          
+
+            if (points >= 5) {
+                victory.Visible = true;
+
             }
         }
 
@@ -51,9 +108,15 @@ namespace PingPong
             if (e.KeyCode == Keys.Space)
             {
                 timer1.Enabled = !(timer1.Enabled);
+                Pause.Visible = !(Pause.Visible);
             }
+
+            }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
             
-            }
         }
+    }
     }
 
